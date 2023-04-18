f4acc47c6cd9210eae597957f445044b7f63e012
Hi there <Hi there@DESKTOP-ER0IR24>
2023-04-18 10:27:00 +0900

    class 이름 수정

---
+++ githubTest1/src/main/java/githubTest1/GithubTest1Vo.java
@@ -0,0 +1,71 @@
+package githubTest1;
+
+import java.sql.Date;
+
+// github test1
+public class GithubTest1Vo {
+	private int num; 
+	private String wirter;
+	private String title;
+	private String content;
+	private Date wdate;
+	
+	public GithubTest1Vo() {
+	}
+	
+	public GithubTest1Vo(int num, String wirter, String title, String content, Date wdate) {
+		super();
+		this.num = num;
+		this.wirter = wirter;
+		this.title = title;
+		this.content = content;
+		this.wdate = wdate;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getWirter() {
+		return wirter;
+	}
+
+	public void setWirter(String wirter) {
+		this.wirter = wirter;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getWdate() {
+		return wdate;
+	}
+
+	public void setWdate(Date wdate) {
+		this.wdate = wdate;
+	}
+
+	@Override
+	public String toString() {
+		return "GithubTest1 [num=" + num + ", wirter=" + wirter + ", title=" + title + ", content=" + content
+				+ ", wdate=" + wdate + "]";
+	}
+	
+}
